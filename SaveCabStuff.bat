e0131269b7942caf044552b33257ad47592995a4
careymcg <carey.mcgilliard@noaa.gov>
2015-03-14 01:50:04 -0700

    This time really updated SaveCabStuff.bat to write to the results folder

--- SaveCabStuff.bat
+++ SaveCabStuff.bat
@@ -1,8 +1,6 @@
-copy cab.cor cab.corYr%1Sim%2Config%3
-copy cab.rep cab.repYr%1Sim%2Config%3
-copy cab.dat cab.datYr%1Sim%2Config%3
-copy cab.ctl cab.ctlYr%1Sim%2Config%3
-copy mvninputFem.txt mvninputFem.txtYr%1Sim%2Config%3
-copy mvninputMale.txt mvninputMale.txtYr%1Sim%2Config%3
-copy mvnoutputFem.txt mvnoutputFem.txtYr%1Sim%2Config%3
-copy mvnoutputMale.txt mvnoutputMale.txtYr%1Sim%2Config%3
+copy cab.cor results\cab.corYr%1Sim%2Config%3
+copy cab.rep results\cab.repYr%1Sim%2Config%3
+copy cab.dat results\cab.datYr%1Sim%2Config%3
+copy cab.ctl results\cab.ctlYr%1Sim%2Config%3
+copy mvninputAll.txt results\mvninputAll.txtYr%1Sim%2Config%3
+copy mvnoutputAll.txt results\mvnoutputAll.txtYr%1Sim%2Config%3
