1727b89ffbb26cc099a828bc835beff2c092dfcb
Jim Ianelli <jim.ianelli@noaa.gov>
2015-07-15 16:19:54 -0700

    Fixed calls to R data and scripts

--- runs/run.bat
+++ runs/run.bat
@@ -2,9 +2,11 @@
 set exec=..\src\build\debug\cab.exe 
 set exec2=..\src\build\debug\main_code_average.exe 
 set exec3=..\src\build\debug\TechInteractions.exe 
+set exec4=..\src\build\debug\main_code.exe 
 :: if EXIST %exec% (mklink /D  %exec% gmacs.exe ) ELSE echo "file missing, compile source code in gmacs\src directory "
 if EXIST %exec% (copy %exec% cab.exe ) ELSE echo "file missing, compile source code in gmacs\src directory "
 if EXIST %exec2% (copy %exec2% main_code_average.exe ) ELSE echo "file missing, compile source code in gmacs\src directory "
 if EXIST %exec3% (copy %exec3% TechInteractions.exe ) ELSE echo "file missing, compile source code in gmacs\src directory "
+if EXIST %exec4% (copy %exec4% main_code.exe ) ELSE echo "file missing, compile source code in gmacs\src directory "
 TechInteractions 
 
