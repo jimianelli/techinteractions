a7e0055cfcd3454ff893a87805ab78dd160d14ee
Kotkot <kotarono@uw.edu>
2015-09-25 14:23:07 -0700

    updated some files to add some flexibility in running simulations and corrected little things

--- runs/run.bat
+++ runs/run.bat
@@ -1,9 +1,9 @@
 @echo off
 if EXIST results (rmdir /S /Q results)
-set exec=..\src\build\debug\cab.exe 
-set exec2=..\src\build\debug\main_code_average.exe 
-set exec3=..\src\build\debug\TechInteractions.exe 
-set exec4=..\src\build\debug\main_code.exe 
+set exec=..\src\\cab.exe 
+set exec2=..\src\main_code_average.exe 
+set exec3=..\src\TechInteractions.exe 
+set exec4=..\src\main_code.exe 
 :: if EXIST %exec% (mklink /D  %exec% gmacs.exe ) ELSE echo "file missing, compile source code in gmacs\src directory "
 if EXIST %exec% (copy %exec% cab.exe ) ELSE echo "file missing, compile source code in gmacs\src directory "
 if EXIST %exec2% (copy %exec2% main_code_average.exe ) ELSE echo "file missing, compile source code in gmacs\src directory "
