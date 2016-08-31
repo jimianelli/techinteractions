21fa50e1fc562d76240bfc988571544f2afe0fd8
Kotkot <kotarono@uw.edu>
2016-08-30 21:02:51 -0700

    important changes to the run batch file

--- runs/run.bat
+++ runs/run.bat
@@ -1,4 +1,18 @@
 @echo off
+
+if EXIST results (
+	goto doall
+) else (
+	goto dontbatch
+)
+
+:doall
+echo Do you want to erase previous run. (yes/no)
+set /p input= 
+if /i %input% == "yes" goto dobatch
+if /i %input% == "no" goto dontbatch
+
+:dobatch
 if EXIST results (rmdir /S /Q results)
 if EXIST profit.dat (del profit.dat)
 if EXIST TruExp_history.dat (del TruExp_history.dat)
@@ -24,3 +38,14 @@ if EXIST %exec3% (copy %exec3% TechInteractions.exe ) ELSE echo "file missing, c
 if EXIST %exec4% (copy %exec4% main_code.exe ) ELSE echo "file missing, compile source code in gmacs\src directory "
 TechInteractions 
 
+:dontbatch
+set exec=..\src\\cab.exe 
+set exec2=..\src\main_code_average.exe 
+set exec3=..\src\TechInteractions.exe 
+set exec4=..\src\main_code.exe 
+:: if EXIST %exec% (mklink /D  %exec% gmacs.exe ) ELSE echo "file missing, compile source code in gmacs\src directory "
+if EXIST %exec% (copy %exec% cab.exe ) ELSE echo "file missing, compile source code in gmacs\src directory "
+if EXIST %exec2% (copy %exec2% main_code_average.exe ) ELSE echo "file missing, compile source code in gmacs\src directory "
+if EXIST %exec3% (copy %exec3% TechInteractions.exe ) ELSE echo "file missing, compile source code in gmacs\src directory "
+if EXIST %exec4% (copy %exec4% main_code.exe ) ELSE echo "file missing, compile source code in gmacs\src directory "
+TechInteractions
